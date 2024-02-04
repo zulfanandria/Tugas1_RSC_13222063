2bf6cea5cc2c2308d4cb58c8bdfe1bdf3e02ed5c
Zulfan Andria Putranto <zulfan.andria@gmail.com>
2024-02-05 02:27:04 +0700

    Formatting

--- No3/03.cpp
+++ No3/03.cpp
@@ -4,11 +4,13 @@ typedef struct
               */
     float Y; /* ordinat */
 } POINT;
+
 void Geser_1(POINT *P, float deltaX, float deltaY)
 {
     (*P).X += deltaX;
     (*P).Y += deltaY;
 }
+
 POINT Geser_2(POINT P, float deltaX, float deltaY)
 {
     P.X += deltaX;
